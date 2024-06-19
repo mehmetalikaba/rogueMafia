6b355476002c4967bf813460cc4d879aeb95dfcd
Halil Eren Yazıcı <halilerenyazici@gmail.com>
2024-06-19 22:30:53 +0300

    işlemeler

--- Assets/halilEren/scripts/oyuncu/oyuncuSaldiriTest.cs
+++ Assets/halilEren/scripts/oyuncu/oyuncuSaldiriTest.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class oyuncuSaldiriTest : MonoBehaviour
 {
+    public bool saldirdi;
+    public float saldiriMenzil;
+    public LayerMask hitLayer;
+    RaycastHit2D hit;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,14 @@ public class oyuncuSaldiriTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetMouseButtonDown(0))
+        {
+            hit = Physics2D.Raycast(transform.position, transform.right, saldiriMenzil, hitLayer);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.name);
+                saldirdi = true;
+            }
+        }
     }
 }
