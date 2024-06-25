d3e333f4b1aaccaa3375ca70ddcba03e729bf3b4
Halil Eren Yazıcı <halilerenyazici@gmail.com>
2024-06-25 17:56:28 +0300

    menü için temel başlangıç

---
+++ Assets/halilEren/scripts/menu/AnaMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnaMenu : MonoBehaviour
+{
+    public Animator darkAnim;
+    public GameObject anaMenu, hakkindaObj, ayarlarObj,yukleniyor,sahneObjeler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void oyna()
+    {
+        darkAnim.SetTrigger("dark");
+
+        anaMenu.SetActive(false);
+        sahneObjeler.SetActive(false);
+        yukleniyor.SetActive(true);
+
+        StartCoroutine(gameStartTime());
+    }
+    public void hakinda()
+    {
+        darkAnim.SetTrigger("dark");
+
+        hakkindaObj.SetActive(true);
+        anaMenu.SetActive(false);
+    }
+    public void ayarlar()
+    {
+        darkAnim.SetTrigger("dark");
+
+        ayarlarObj.SetActive(true);
+        anaMenu.SetActive(false);
+    }
+    public void geriDon()
+    {
+        darkAnim.SetTrigger("dark");
+
+        anaMenu.SetActive(true);
+        ayarlarObj.SetActive(false);
+        hakkindaObj.SetActive(false);
+    }
+    IEnumerator gameStartTime()
+    {
+        yield return new WaitForSeconds(6);
+        SceneManager.LoadScene("test");
+    }
+}
