5c4b24f883d55c4c03a255a82b1f22e1a4e430ae
Halil Eren Yazıcı <halilerenyazici@gmail.com>
2024-07-03 13:21:16 +0300

    hareketli çim

---
+++ Assets/halilEren/scripts/diğerleri/cim.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cim : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("oyuncu")||collision.gameObject.CompareTag("dusman"))
+        {
+            if (collision.transform.position.x > transform.position.x)
+            {
+                animator.SetTrigger("cimSol");
+
+            }
+            if (collision.transform.position.x < transform.position.x)
+            {
+                animator.SetTrigger("cimSag");
+
+            }
+        }
+    }
+}
