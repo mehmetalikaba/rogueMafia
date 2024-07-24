e86c822db7614c37e28a3e378bdd2af035d328b1
MehmetAliKABA <mehmetalikaba06@gmail.com>
2024-07-24 14:56:56 +0300

    Update sesKontrol.cs

--- Assets/mehmetAli/scripts/digerSistemleri/sesKontrol.cs
+++ Assets/mehmetAli/scripts/digerSistemleri/sesKontrol.cs
@@ -45,19 +45,19 @@ public class sesKontrol : MonoBehaviour
 
         for (int i = 0; i < oyunMuzikleri.Length; i++)
         {
-            oyunMuzikleri[i].volume = sesSeviyeleri[0];
+            oyunMuzikleri[i].volume *= sesSeviyeleri[0];
         }
         for (int i = 0; i < ortamSesleri.Length; i++)
         {
-            ortamSesleri[i].volume = sesSeviyeleri[1];
+            ortamSesleri[i].volume *= sesSeviyeleri[1];
         }
         for (int i = 0; i < menuMuzigi.Length; i++)
         {
-            menuMuzigi[i].volume = sesSeviyeleri[2];
+            menuMuzigi[i].volume *= sesSeviyeleri[2];
         }
         for (int i = 0; i < sesEfektleri.Length; i++)
         {
-            sesEfektleri[i].volume = sesSeviyeleri[3];
+            sesEfektleri[i].volume *= sesSeviyeleri[3];
         }
 
     }
