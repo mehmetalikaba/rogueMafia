03d71eb98e45e59d88b5e2a231b09f05ea1a3e83
Halil Eren Yazıcı <halilerenyazici@gmail.com>
2024-07-25 18:58:19 +0300

    Update objeOlusturucu.cs

--- Assets/halilEren/scripts/oluşturucular/objeOlusturucu.cs
+++ Assets/halilEren/scripts/oluşturucular/objeOlusturucu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class objeOlusturucu : MonoBehaviour
 {
+    public int ihtimal;
     public GameObject[] gameObjects;
     public Transform[] transforms;
     // Start is called before the first frame update
@@ -12,7 +13,12 @@ public class objeOlusturucu : MonoBehaviour
         for (int i = 0; i < transforms.Length; i++)
         {
             int a = Random.Range(0, gameObjects.Length);
-            Instantiate(gameObjects[a], transforms[i].transform.position, transforms[i].transform.rotation);
+            int b = Random.Range(0, 100);
+            if(b<=ihtimal)
+            {
+                Instantiate(gameObjects[a], transforms[i].transform.position, transforms[i].transform.rotation);
+
+            }
         }
     }
 
