86e702ab8800bb9478834cd888830ae56151fa44
Halil Eren Yazıcı <halilerenyazici@gmail.com>
2024-07-25 18:59:59 +0300

    Update objeOlusturucu.cs

--- Assets/halilEren/scripts/oluşturucular/objeOlusturucu.cs
+++ Assets/halilEren/scripts/oluşturucular/objeOlusturucu.cs
@@ -10,6 +10,10 @@ public class objeOlusturucu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if(ihtimal== 0)
+        {
+            ihtimal = 100;
+        }
         for (int i = 0; i < transforms.Length; i++)
         {
             int a = Random.Range(0, gameObjects.Length);
