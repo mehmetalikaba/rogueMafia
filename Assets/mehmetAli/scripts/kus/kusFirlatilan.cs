6be11ef87501ccfec0c8b1f519a9d2fb5b217cc6
MehmetAliKABA <mehmetalikaba06@gmail.com>
2024-08-23 09:13:43 +0300

    kus bitti

---
+++ Assets/mehmetAli/scripts/kus/kusFirlatilan.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class KusFirlatilan : MonoBehaviour
+{
+    public float hareketHizi = 5.0f;
+    public LayerMask dusmanLayer;
+    private Transform hedefDusman;
+    private bool hedefBelirlendi = false;
+
+    void Start()
+    {
+        BulEnYakinDusman();
+    }
+
+    void Update()
+    {
+        if (hedefBelirlendi)
+            transform.position = Vector3.MoveTowards(transform.position, hedefDusman.position, hareketHizi * Time.deltaTime);
+    }
+
+    void BulEnYakinDusman()
+    {
+        dusmanHasar[] dusmanHasarlar = FindObjectsOfType<dusmanHasar>();
+
+        Transform enYakinDusman = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (dusmanHasar dusman in dusmanHasarlar)
+        {
+            float distanceToDusman = Vector3.Distance(transform.position, dusman.transform.position);
+            if (distanceToDusman < minDistance)
+            {
+                minDistance = distanceToDusman;
+                enYakinDusman = dusman.transform;
+            }
+        }
+        if (enYakinDusman != null)
+        {
+            hedefDusman = enYakinDusman;
+            hedefBelirlendi = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("dusman"))
+        {
+            Debug.Log(collision.gameObject.name);
+            collision.gameObject.GetComponent<dusmanHasar>().hasarAl(15, "kusFirlatilan");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/mehmetAli/scripts/kus/kusKontrol.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class kusKontrol : MonoBehaviour
+{
+    public GameObject oyuncu, kusFirlatilan;
+    public float yalpalamaMiktari, firlatmaTimer, yalpalamaHizi, firlatmaSuresi, yumuşatmaHizi = 0.125f, takipHizi = 5.0f;
+    public bool yakindaDusmanVar, firlatildiMi;
+
+    public Vector3 soldaDurus = new Vector3(-1f, 1f, 0f);
+    public Vector3 sagdaDurus = new Vector3(1f, 1f, 0f);
+    private Vector3 hedefPozisyon;
+    private oyuncuHareket oyuncuHareket;
+
+    void Start()
+    {
+        oyuncuHareket = oyuncu.GetComponent<oyuncuHareket>();
+        firlatmaTimer = 0;
+    }
+
+    void Update()
+    {
+        if (oyuncuHareket.sagaBakiyor)
+            hedefPozisyon = oyuncu.transform.position + soldaDurus;
+        else
+            hedefPozisyon = oyuncu.transform.position + sagdaDurus;
+
+        hedefPozisyon += new Vector3(
+            Mathf.Sin(Time.time * yalpalamaHizi) * yalpalamaMiktari,
+            Mathf.Cos(Time.time * yalpalamaHizi) * yalpalamaMiktari,
+            0f
+        );
+
+        Vector3 yeniPozisyon = Vector3.Lerp(transform.position, hedefPozisyon, yumuşatmaHizi);
+        transform.position = Vector3.MoveTowards(transform.position, yeniPozisyon, takipHizi * Time.deltaTime);
+
+        if (hedefPozisyon.x > transform.position.x)
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        else
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+
+        dusmanVarMi();
+
+        if (firlatildiMi)
+        {
+            firlatmaTimer -= Time.deltaTime;
+            if (firlatmaTimer <= 0)
+            {
+                firlatildiMi = false;
+                firlatmaTimer = firlatmaSuresi;
+            }
+        }
+    }
+
+    void dusmanVarMi()
+    {
+        if (!firlatildiMi)
+        {
+            dusmanHasar[] dusmanlar = FindObjectsOfType<dusmanHasar>();
+            foreach (var dusman in dusmanlar)
+            {
+                float distance = Vector3.Distance(dusman.transform.position, oyuncu.transform.position);
+                if (distance <= 5)
+                {
+                    yakindaDusmanVar = true;
+                    if (!firlatildiMi)
+                    {
+                        firlatildiMi = true;
+                        Instantiate(kusFirlatilan, transform.position, transform.rotation);
+                    }
+                    break;
+                }
+                else
+                    yakindaDusmanVar = false;
+            }
+        }
+    }
+}
