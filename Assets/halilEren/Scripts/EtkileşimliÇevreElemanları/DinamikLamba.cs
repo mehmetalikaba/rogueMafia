6fa1a9cde5593b4743cb9ddbd4b73c40a56c2a6b
Halil Eren Yazıcı <halilerenyazici@gmail.com>
2024-09-02 18:56:47 +0300

    çevresel etkileşimler başlama + shader deneme

--- Assets/halilEren/Scripts/EtkileşimliÇevreElemanları/DinamikLamba.cs
+++ Assets/halilEren/Scripts/EtkileşimliÇevreElemanları/DinamikLamba.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class DinamikLamba : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("oyuncu") || collision.gameObject.CompareTag("dusman"))
+        {
+            if (collision.transform.position.x > transform.position.x)
+            {
+                animator.SetTrigger("solaHareket");
+
+            }
+            if (collision.transform.position.x < transform.position.x)
+            {
+                animator.SetTrigger("sagaHareket");
+
+            }
+        }
     }
 }
