80ba13bbbbe2b975fab0643ad5c79acc32c281c5
Halil Eren Yazıcı <halilerenyazici@gmail.com>
2024-09-04 14:47:58 +0300

    kamp ateşi fizik düzeni

---
+++ Assets/halilEren/Scripts/EtkileşimliÇevreElemanları/kesilebilirBitki.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kesilebilirBitki : MonoBehaviour
+{
+    public oyuncuSaldiriTest oyuncuSaldiriTest;
+    public bool oyuncuYakin;
+
+    SpriteRenderer spriteRenderer;
+    public Sprite kesilmisCizim;
+    public void Start()
+    {
+        oyuncuSaldiriTest = FindObjectOfType<oyuncuSaldiriTest>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void FixedUpdate()
+    {
+        oyuncuYakin = Physics2D.OverlapCircle(transform.position, 2f, LayerMask.GetMask("Oyuncu"));
+
+        if (Input.GetKeyDown(tusDizilimleri.instance.tusIsleviGetir("solTikTusu")) && oyuncuYakin && !oyuncuSaldiriTest.yumruk1)
+            kesildi();
+    }
+    public void kesildi()
+    {
+        spriteRenderer.sprite = kesilmisCizim;
+    }
+}
