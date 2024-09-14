c1571d9791071c2b705c8b4f454c9ee23ef93f53
Halil Eren Yazıcı <halilerenyazici@gmail.com>
2024-09-14 13:57:08 +0300

    push

---
+++ Assets/_GistSahneleri/RestartScene.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Keypad0))
+        {
+            RestartSceneFonk();
+        }
+        if(Input.GetKeyUp(KeyCode.Keypad9))
+        {
+            Menuye();
+        }
+    }
+    // Bu fonksiyon mevcut sahneyi yeniden başlatır
+    public void RestartSceneFonk()
+    {
+        // Mevcut sahnenin adı
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        // Mevcut sahneyi yeniden yükle
+        SceneManager.LoadScene(currentSceneName);
+    }
+    public void Menuye()
+    {
+        SceneManager.LoadScene("_GistMenü");
+    }
+}
